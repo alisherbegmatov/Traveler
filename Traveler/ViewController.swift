9b37e2ca4af9bee65522e445b7ad2237d3ea8bcc
Alisher Begmatov <alisherbegmatov@yandex.com>
2020-01-28 17:47:43 -0800

    Initial Commit

---
+++ Traveler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Traveler
+//
+//  Created by Alisher Begmatov on 1/28/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
